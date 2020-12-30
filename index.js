b31bf1203546ecac832935eeb533bc8965012e96
Michael Corey Munn <coreymunn3@gmail.com>
2020-12-30 11:58:44 -0500

    heroku build deploy

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
+// build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
